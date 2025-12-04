f6ee60b2305f93903264a11a5ac7bd20de30e056
Jorge <jbarrera-simental@csumb.edu>
2025-12-03 20:46:46 -0800

    Added route to retrieve group events

--- src/main/java/com/example/EventLink/controller/GroupEventsController.java
+++ src/main/java/com/example/EventLink/controller/GroupEventsController.java
@@ -3,11 +3,12 @@ package com.example.EventLink.controller;
 import com.example.EventLink.entity.GroupEventsEntity;
 import com.example.EventLink.repository.GroupEventsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/groupEvents")
@@ -24,5 +25,13 @@ public class GroupEventsController {
         return ResponseEntity.ok(savedEvent);
     }
 
+    @GetMapping("/{groupId}")
+    public ResponseEntity<List<Long>> getEventIdsByGroup(@PathVariable Long groupId) {
+        List<Long> eventIds = groupEventsRepository.findByIdGroupId(groupId)
+                .stream()
+                .map(entity -> entity.getId().getEventId())
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(eventIds, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/example/EventLink/repository/GroupEventsRepository.java
+++ src/main/java/com/example/EventLink/repository/GroupEventsRepository.java
@@ -4,5 +4,8 @@ import com.example.EventLink.entity.GroupEventsEntity;
 import com.example.EventLink.entity.GroupMembersId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupEventsRepository extends JpaRepository<GroupEventsEntity, GroupMembersId> {
+    List<GroupEventsEntity> findByIdGroupId(Long groupId);
 }
